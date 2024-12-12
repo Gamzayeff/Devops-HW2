d79a4955f52f9d5e725a2f5c19e1053396d8fb1f
Hemze_bk91 <Hemze_bk91@STHQ012E-07>
2024-12-12 09:12:20 +0400

    Add project files.

---
+++ Redis PUB-SUB Chating Homework2/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis_PUB_SUB_Chating_Homework2.Models;
+using Redis_PUB_SUB_Chating_Homework2.Services.Abstracts;
+using Redis_PUB_SUB_Chating_Homework2.Services.Concretes;
+using System.Diagnostics;
+
+namespace Redis_PUB_SUB_Chating_Homework2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRedisChannelService _redisChannelService;
+        public HomeController(IRedisChannelService redisChannelService)
+        {
+            _redisChannelService = redisChannelService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string selectedChannel = null)
+        {
+            var channels = await _redisChannelService.GetAllChannelsAsync();
+            ViewBag.Channels = channels;
+
+            if (!string.IsNullOrEmpty(selectedChannel))
+            {
+                var messages = await _redisChannelService.GetChannelMessagesAsync(selectedChannel);
+                ViewBag.SelectedChannelMessages = messages;
+                ViewBag.SelectedChannelName = selectedChannel;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChannel(string channelName)
+        {
+            await _redisChannelService.CreateChannelAsync(channelName);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SelectChannel(string channelName)
+        {
+            var messages = await _redisChannelService.GetChannelMessagesAsync(channelName);
+            ViewBag.SelectedChannelMessages = messages;
+            ViewBag.SelectedChannelName = channelName;
+
+            var channels = await _redisChannelService.GetAllChannelsAsync();
+            ViewBag.Channels = channels;
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(string channelName, string message)
+        {
+            if (!string.IsNullOrWhiteSpace(channelName) && !string.IsNullOrWhiteSpace(message))
+            {
+                await _redisChannelService.AddMessageToChannelAsync(channelName, message);
+            }
+
+            return RedirectToAction("Index", new { selectedChannel = channelName });
+        }
+
+    }
+}
---
+++ Redis PUB-SUB Chating Homework2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Redis_PUB_SUB_Chating_Homework2.Services.Abstracts;
+using Redis_PUB_SUB_Chating_Homework2.Services.Concretes;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(new ConfigurationOptions
+    {
+        EndPoints = { { "", 0 } },
+        User = "",
+        Password = ""
+    }));
+
+builder.Services.AddScoped<IRedisChannelService,RedisChannelService>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
